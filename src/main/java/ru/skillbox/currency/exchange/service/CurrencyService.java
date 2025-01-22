d54c7cadb0b09fd118dc868850343880c15323a2
Marat <clever1mistory@gmail.com>
2025-01-22 14:04:48 +0300

    entity updated & new methods

---
+++ src/main/java/ru/skillbox/currency/exchange/service/CurrencyService.java
@@ -0,0 +1,52 @@
+package ru.skillbox.currency.exchange.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import ru.skillbox.currency.exchange.dto.CurrencyDto;
+import ru.skillbox.currency.exchange.entity.Currency;
+import ru.skillbox.currency.exchange.mapper.CurrencyMapper;
+import ru.skillbox.currency.exchange.repository.CurrencyRepository;
+import ru.skillbox.currency.exchange.xmlparser.ValCurs;
+import ru.skillbox.currency.exchange.entity.Valute;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+    private final CurrencyMapper mapper;
+    private final CurrencyRepository repository;
+
+    public CurrencyDto getById(Long id) {
+        log.info("CurrencyService method getById executed");
+        Currency currency = repository.findById(id).orElseThrow(() -> new RuntimeException("Currency not found with id: " + id));
+        return mapper.convertToDto(currency);
+    }
+
+    public List<CurrencyDto> getAllCurrencies() {
+        log.info("CurrencyService method getAllCurrencies executed");
+        return repository.findAll().stream()
+                .map(mapper::convertToDto)
+                .collect(Collectors.toList());
+    }
+
+    public Double convertValue(Long value, Long numCode) {
+        log.info("CurrencyService method convertValue executed");
+        Currency currency = repository.findByIsoNumCode(numCode);
+        return value * currency.getValue();
+    }
+
+    public CurrencyDto create(CurrencyDto dto) {
+        log.info("CurrencyService method create executed");
+        return mapper.convertToDto(repository.save(mapper.convertToEntity(dto)));
+    }
+}
